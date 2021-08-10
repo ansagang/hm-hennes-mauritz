12a18884a83e3f2b01dbe25659a59c4b856801e0
ansagang <ansagaang@gmail.com>
2021-08-10 15:24:32 +0600

    chore: Добавил категории протестил апишку

--- src/screens/Catalog.jsx
+++ src/screens/Catalog.jsx
@@ -4,7 +4,10 @@ import { useEffect } from "react"
 function Catalog() {
     const [categories, setCategories] = useState([])
     const [products, setProducts] = useState([])
-    
+    const [categorieValue, setCategorieValue] = useState()
+    const [activeSubCategories, setActiveSubCategories] = useState()
+    console.log(categorieValue);
+
     useEffect(() => {
         fetch("https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/categories/list?lang=en&country=asia2", {
             "method": "GET",
@@ -16,11 +19,12 @@ function Catalog() {
             .then(response => response.json())
             .then(data => {
                 setCategories(data)
+                console.log(data);
             })
     }, [])
 
     useEffect(() => {
-        fetch("https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=asia2&lang=en&currentpage=0&pagesize=30&categories=men_all&concepts=H%26M%20MAN", {
+        fetch(categorieValue ? "https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=asia2&lang=en&currentpage=0&pagesize=30&categories=" + categorieValue + "_all" : "https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=asia2&lang=en&currentpage=0&pagesize=30", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "ef24b3f69dmsh1705ad4c20add74p1d6f35jsncc375f062f3f",
@@ -29,9 +33,10 @@ function Catalog() {
         })
             .then(response => response.json())
             .then(data => {
-                setProducts(data)
+                setProducts(data.results)
+                console.log(data);
             })
-    }, [])
+    }, [categorieValue])
 
     return (
         <div className="catalog">
@@ -39,17 +44,29 @@ function Catalog() {
                 <div className="catalog-inner inner">
                     <div className="catalog-categories">
                         {
-                            categories.map((categorie) => (
-                                <button className="catalog-categorie">{categorie.CatName}</button>
+                            categories.map((categorie, i) => (
+                                <div className={activeSubCategories === i ? 'catalog-categorie active' : 'catalog-categorie'} onMouseLeave={() => setActiveSubCategories()} onMouseEnter={() => setActiveSubCategories(i)} key={i}>
+                                    <button style={categorieValue === categorie.CategoryValue ? { color: '#c11a2b' } : null} onClick={() => setCategorieValue(categorie.CategoryValue)} className="catalog-categorie-button">{categorie.CatName}</button>
+                                    <div className="catalog-categorie-subcategories">
+                                        {
+                                            categorie.CategoriesArray ?
+                                                console.log(categorie.CategoriesArray)
+                                                :
+                                                console.log(categorie.CategoriesArray)
+                                        }
+                                    </div>
+                                </div>
                             ))
                         }
                     </div>
                     <div className="catalog-list">
-                        {/* {
-                            products.map((product) => (
-                                <div className="af">{product.results.name}</div>
+                        {
+                            products ? products.map((product) => (
+                                <div className="af">{product.name}</div>
                             ))
-                        } */}
+                                :
+                                <div className="hlo">bb</div>
+                        }
                     </div>
                 </div>
             </div>
